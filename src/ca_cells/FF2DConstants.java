74c1923aa37a8c736f49bc6ee34d25009f205ce6
Joshua O'Leary <jmoleary1@sheffield.ac.uk>
2016-03-10 16:03:55 +0000

    Use terrain distribution

--- src/ca_cells/FF2DConstants.java
+++ src/ca_cells/FF2DConstants.java
@@ -4,7 +4,7 @@ package ca_cells;
  * Created by joshua on 10/03/16.
  */
 public class FF2DConstants {
-    public static final boolean USE_TERRAIN_FUEL_DISTRIBUTION = false;
+    public static final boolean USE_TERRAIN_FUEL_DISTRIBUTION = true;
 
     public enum MODE {
         SIMPLE, REFRACTORY, PROBABILISTIC
